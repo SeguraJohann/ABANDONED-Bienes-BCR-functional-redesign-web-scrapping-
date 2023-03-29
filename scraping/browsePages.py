5621aadba4fa1d8390540a1278da50fe3e62eafb
SeguraJohann <johann.segura.chacon@gmail.com>
2023-03-28 18:09:41 -0600

    Hago getUrls headless y lo renombro a nextpage

--- scraping/browsePages.py
+++ scraping/browsePages.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-def getUrls(url):
 
-    driver = webdriver.Chrome()
+
+def nextPage(url):
+
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
-    driver.maximize_window()
     time.sleep(0.5)
     response = requests.get(url)
     nextButton = driver.find_element(By.CLASS_NAME, "page-item.next").find_element(By.TAG_NAME, "a")
@@ -18,4 +22,4 @@ def getUrls(url):
     nextButton.click()
     time.sleep(0.5)
     
-getUrls("https://ventadebienes.bancobcr.com/wps/portal/bcrb/bcrbienes/bienes/Casas?&tipo_propiedad=1")
+nextPage("https://ventadebienes.bancobcr.com/wps/portal/bcrb/bcrbienes/bienes/Casas?&tipo_propiedad=1")
