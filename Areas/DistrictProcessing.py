eabdf84cfd652cfbfac722e418eadb687cd8393a
SeguraJohann <johann.segura.chacon@gmail.com>
2023-06-11 21:19:00 -0600

    Pandas test, improved Cantons ID

--- Areas/DistrictProcessing.py
+++ Areas/DistrictProcessing.py
@@ -2,25 +2,34 @@ import pandas as pd
 import psycopg2
 
 #Creates a df the Province column only, deletes duplicates values and reset the indexes from 1 to 7
-data = pd.read_csv("Districts_of_Costa_Rica_1.csv")
-provincias = data["Province"].copy()
+data = pd.read_csv('Districts_of_Costa_Rica_1.csv')
+provincias = pd.DataFrame(data['Province'].copy())
 provincias = provincias.drop_duplicates()
 provincias = provincias.reset_index(drop=True)
 provincias.index = provincias.index + 1
+provincias['Index'] = provincias.index
 
 
-canton = data[["Province","Canton"]].copy()
+canton = data[['Province','Canton']].copy()
 canton = canton.drop_duplicates()
 canton = canton.reset_index(drop=True)
 canton.index = canton.index + 1
 #print(data)
-#print(provincias)
+'''print(provincias)
+print(provincias[provincias['Province'] == 'San José'])
 #print(type(provincias))
 
-#for indx, values in provincias.items():
-#    print('index: ', indx, 'value: ', values)
+
 
 for indx, values in canton.iterrows():
     #print(indx, values)
     #print(indx)
     print(values["Province"],values["Canton"])
+    print(provincias[provincias['Province'] == values["Province"]]['Index'])'''
+#print(provincias)
+for _, values in provincias.iterrows():
+
+    canton.loc[canton['Province'] == values['Province'],'Province'] = values['Index']
+    print(canton[canton['Province'] == values['Province']])
+
+print(canton[:])
